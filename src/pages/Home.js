2e9c70fe716ccc7435da6332d46ab8557cf07d22
Roh Geun Yeok <daveno@naver.com>
2025-11-20 17:06:24 +0900

    ...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,47 @@
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(()=>{
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+      .from('todos')
+      .select()
+
+      if(error) {
+        setFetchError('Could not fetch data.')
+        setSmoothies(null);
+        console.log(error)
+      }
+
+      if(data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, []);
+
   return (
     <div className="page home">
       <h2>Home</h2>
+
+      <div>
+        {fetchError && (<p>{fetchError}</p>)}
+        {smoothies && (
+          <div className="smoothies">
+            {smoothies.map(smoothie=>(
+              <div>x</div>
+            ))}
+          </div>
+          
+        )}
+      </div>
     </div>
   )
 }
